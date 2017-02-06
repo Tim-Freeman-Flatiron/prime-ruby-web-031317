b0d4ec0073bccf169b5cd310c7d868fa5c0613b8
Tim Freeman <d.timothy.freeman@gmail.com>
2017-02-06 16:07:27 -0500

    Prime Ruby -- Passed Tests

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(int)
+	prime = true
+	if int <= 1
+		prime = false
+	else
+		for i in (2...int).to_a
+			if (int % i == 0)
+				prime = false
+				break
+			end
+		end
+	end
+	prime
+end
